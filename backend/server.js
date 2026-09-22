a9ea2b92160ee4c2846f8eaf5f846e95fdd16459
Uriel Ortiz <urielzaidtovarortiz@gmail.com>
2026-09-22 17:17:14 -0600

    Actualizar ruta del login

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 app.get("/", (req, res) => {
-    res.sendFile("indexr.html", { root: ".." });
+    res.sendFile("index.html", { root: ".." });
 });
 
 app.use(express.static("..", { index: false }));
